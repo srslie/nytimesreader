e161cc03cd2ea0aaadb71ba1488d1d80438cf879
srslie <aliceruppert@gmail.com>
2022-07-28 10:41:48 -0500

    Remove unneeded props

--- src/components/Article.js
+++ src/components/Article.js
@@ -4,10 +4,8 @@ import '../styles/Article.css';
 export const Article = (props) => {
   return (
     <div className="article">
-      <p>Article</p>
       <h3>{props.title}</h3>
       <h4>{props.byline}</h4>
-      <h4>{props.published_date}</h4>
       <p>{props.abstract}</p>
     </div>
   )
